b9bf0e5cc327d17c8b63da3ddb316f3cb0a6cc86
Frank D Evans <frankdevans@yahoo.com>
2015-07-20 14:47:13 -0500

    add gamelog integration into EDA

--- eda.R
+++ eda.R
@@ -13,8 +13,8 @@ atbats <- tbl_df(read.table(file = './data_parsed/atbats_2014.csv',
 games <- read.table(file = './data_parsed/game_2014.csv',
                       header = TRUE, sep = '|', quote = '')
 
-game_actions <- read.table(file = './data_parsed/game_actions_2014.csv',
-                           header = TRUE, sep = '|', quote = '')
+game_actions <- tbl_df(read.table(file = './data_parsed/game_actions_2014.csv',
+                                  header = TRUE, sep = '|', quote = ''))
 
 game_hits <- read.table(file = './data_parsed/game_hits_2014.csv',
                         header = TRUE, sep = '|', quote = '')
@@ -30,8 +30,6 @@ game_teams <- read.table(file = './data_parsed/game_teams_2014.csv',
 
 game_umpires <- read.table(file = './data_parsed/game_umpires_2014.csv',
                            header = TRUE, sep = '|', quote = '')
-
-
 
 
 
@@ -118,7 +116,74 @@ pitches %>%
 
 
 
+# Probability of Winning given runs scored
+game_logs <- tbl_df(read.table(file = './data/game_logs/GL2014.TXT',
+                               header = FALSE,
+                               sep = ','))
 
+wl_logs <- game_logs %>%
+    select(V4, V7, V10, V11, V23, V51) %>%
+    rename(team_visit = V4,
+           team_home = V7,
+           score_visit = V10,
+           score_home = V11,
+           hits_visit = V23,
+           hits_home = V51) %>%
+    mutate(winner_visit = (score_visit > score_home),
+           winner_home = !winner_visit)
+
+wl_logs %>%
+    select(score_visit, winner_visit) %>%
+    rename(score = score_visit,
+           win = winner_visit)
+
+wl_logs %>%
+    select(score_home, winner_home) %>%
+    rename(score = score_home,
+           win = winner_home)
+
+bind_rows(wl_logs %>%
+              select(score_visit, winner_visit) %>%
+              rename(score = score_visit,
+                     win = winner_visit),
+          wl_logs %>%
+              select(score_home, winner_home) %>%
+              rename(score = score_home,
+                     win = winner_home)
+          ) %>%
+    group_by(score) %>%
+    summarise(games = n(),
+              win_prob = mean(win)) %>%
+    ungroup() %>%
+    ggplot(data = .) +
+    geom_line(mapping = aes(x = score, y = win_prob), color = 'steelblue', size = 2) +
+    annotate(geom = 'segment', 
+             x = 0, y = 0.5, xend = 20, yend = 0.5, 
+             color = 'black', size = 1.5, linetype = 'longdash') +
+    labs(x = 'Runs Scored in Game',
+         y = 'Probability of Winning Game')
+
+
+bind_rows(wl_logs %>%
+              select(hits_visit, winner_visit) %>%
+              rename(hits = hits_visit,
+                     win = winner_visit),
+          wl_logs %>%
+              select(hits_home, winner_home) %>%
+              rename(hits = hits_home,
+                     win = winner_home)
+          ) %>%
+    group_by(hits) %>%
+    summarise(games = n(),
+              win_prob = mean(win)) %>%
+    ungroup() %>%
+    ggplot(data = .) +
+    geom_line(mapping = aes(x = hits, y = win_prob), color = 'darkred', size = 2) +
+    annotate(geom = 'segment', 
+             x = 0, y = 0.5, xend = 25, yend = 0.5, 
+             color = 'black', size = 1.5, linetype = 'longdash') +
+    labs(x = 'Hits in Game',
+         y = 'Probability of Winning Game')
 
 
 
