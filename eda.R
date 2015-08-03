2a2ec99b346f22ab35c9f031dd1018c1790951c3
frankdevans <frankdevans@yahoo.com>
2015-08-02 21:08:12 -0500

    add scouting report with TODO, pitch bins

--- eda.R
+++ eda.R
@@ -31,7 +31,10 @@ game_teams <- read.table(file = './data_parsed/game_teams_2014.csv',
 game_umpires <- read.table(file = './data_parsed/game_umpires_2014.csv',
                            header = TRUE, sep = '|', quote = '')
 
-
+bin_pitch <- read.table(file = './data/bin_pitch_des.csv',
+                        header = TRUE,
+                        sep = '|',
+                        stringsAsFactors = FALSE)
 
 
 
@@ -103,7 +106,7 @@ pitches %>%
     scale_color_brewer(palette = 'Set1') +
     annotate('rect', xmin = -0.83, xmax = 0.83, ymin = 1.52, ymax = 3.42,
              color = 'black', size = 1.25, alpha = 0) +
-    xlim(-2,2) + ylim(1,4) +
+    xlim(-3.5,3.5) + ylim(1,4) +
     theme_classic() +
     theme(axis.text = element_blank(),
           axis.title = element_blank(),
@@ -111,6 +114,102 @@ pitches %>%
           axis.ticks = element_blank(),
           legend.position = 'bottom')
     ggsave(filename = './plots/strikes_by_stance.png', width = 150, height = 100, units = 'mm', dpi = 500)
+
+
+
+
+
+# TODO: Pitch Lens recreation
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Pitch Bins
+pitches %>%
+    group_by(pitch_des) %>%
+    summarize(pitches = n()) %>%
+    inner_join(y = bin_pitch, by = 'pitch_des') %>%
+    arrange(desc(pitches)) %>%
+    ggplot(data = .) +
+    geom_bar(mapping = aes(x = pitch_des, y = pitches), stat = 'identity') +
+    theme_minimal() +
+    theme(axis.text.x = element_text(angle = 90, hjust = 1),
+          axis.text.y = element_blank(),
+          axis.title.y = element_blank(),
+          axis.line.y = element_blank(),
+          axis.ticks.y = element_blank()) +
+    labs(x = 'Type of Pitch')
+    ggsave(filename = './plots/bins_pitches_bw.png', width = 150, height = 100, units = 'mm', dpi = 500)
+
+
+pitches %>%
+    group_by(pitch_des) %>%
+    summarize(pitches = n()) %>%
+    inner_join(y = bin_pitch, by = 'pitch_des') %>%
+    arrange(desc(pitches)) %>%
+    ggplot(data = .) +
+    geom_bar(mapping = aes(x = pitch_des, y = pitches, fill = outcome_bin), stat = 'identity') +
+    scale_fill_brewer(palette = 'Set1') +
+    theme_minimal() +
+    theme(axis.text.x = element_text(angle = 90, hjust = 1),
+          axis.text.y = element_blank(),
+          axis.title.y = element_blank(),
+          axis.line.y = element_blank(),
+          axis.ticks.y = element_blank()) +
+    labs(x = 'Type of Pitch')
+    ggsave(filename = './plots/bins_pitches_rgb.png', width = 150, height = 100, units = 'mm', dpi = 500)
+
+
+pitches %>%
+    inner_join(y = bin_pitch, by = 'pitch_des') %>%
+    group_by(outcome_bin) %>%
+    summarize(pitches = n()) %>%
+    ggplot(data = .) +
+    geom_bar(aes(x = outcome_bin, y = pitches, fill = outcome_bin), stat = 'identity') +
+    scale_fill_brewer(palette = 'Set1') +
+    theme(axis.text.y = element_blank(),
+          axis.title.y = element_blank(),
+          axis.line.y = element_blank(),
+          axis.ticks.y = element_blank()) +
+    labs(x = 'Outcome of Pitch')
+    ggsave(filename = './plots/bins_outcome_nopct.png', width = 150, height = 100, units = 'mm', dpi = 500)
+
+
+pitches %>%
+    inner_join(y = bin_pitch, by = 'pitch_des') %>%
+    group_by(outcome_bin) %>%
+    summarize(pitches = n()) %>%
+    ungroup() %>%
+    arrange(desc(pitches)) %>%
+    group_by() %>%
+    mutate(total = sum(pitches)) %>%
+    ungroup() %>%
+    mutate(pct = pitches / total,
+           pct_friendly = paste(round(x = pct * 100, digits = 1), '%')) %>%
+    ggplot(data = .) +
+    geom_bar(aes(x = outcome_bin, y = pitches, fill = outcome_bin), stat = 'identity') +
+    scale_fill_brewer(palette = 'Set1') +
+    geom_text(mapping = aes(x = outcome_bin, y = pitches - 10000, label = pct_friendly), fontface = 'bold')
+    theme(axis.text.y = element_blank(),
+          axis.title.y = element_blank(),
+          axis.line.y = element_blank(),
+          axis.ticks.y = element_blank()) +
+    labs(x = 'Outcome of Pitch')
+    ggsave(filename = './plots/bins_outcome_pct.png', width = 150, height = 100, units = 'mm', dpi = 500)
+
 
 
 
@@ -156,12 +255,35 @@ bind_rows(wl_logs %>%
               win_prob = mean(win)) %>%
     ungroup() %>%
     ggplot(data = .) +
+    geom_bar(mapping = aes(x = score, y = games), stat = 'identity', fill = 'steelblue') +
+    geom_line(mapping = aes(x = score, y = win_prob), color = 'steelblue', size = 2) +
+    labs(x = 'Runs Scored in Game',
+         y = 'Number of Game Occurrences')
+    ggsave(filename = './plots/hist_win_runs.png', width = 150, height = 100, units = 'mm', dpi = 500)
+
+
+bind_rows(wl_logs %>%
+              select(score_visit, winner_visit) %>%
+              rename(score = score_visit,
+                     win = winner_visit),
+          wl_logs %>%
+              select(score_home, winner_home) %>%
+              rename(score = score_home,
+                     win = winner_home)
+        ) %>%
+    group_by(score) %>%
+    summarise(games = n(),
+              win_prob = mean(win)) %>%
+    ungroup() %>%
+    ggplot(data = .) +
     geom_line(mapping = aes(x = score, y = win_prob), color = 'steelblue', size = 2) +
     annotate(geom = 'segment', 
              x = 0, y = 0.5, xend = 20, yend = 0.5, 
              color = 'black', size = 1.5, linetype = 'longdash') +
     labs(x = 'Runs Scored in Game',
          y = 'Probability of Winning Game')
+    ggsave(filename = './plots/prob_win_runs.png', width = 150, height = 100, units = 'mm', dpi = 500)
+
 
 
 bind_rows(wl_logs %>%
@@ -184,6 +306,63 @@ bind_rows(wl_logs %>%
              color = 'black', size = 1.5, linetype = 'longdash') +
     labs(x = 'Hits in Game',
          y = 'Probability of Winning Game')
+    ggsave(filename = './plots/prob_win_hits.png', width = 150, height = 100, units = 'mm', dpi = 500)
+
+
+bind_rows(wl_logs %>%
+              select(hits_visit, winner_visit) %>%
+              rename(hits = hits_visit,
+                     win = winner_visit),
+          wl_logs %>%
+              select(hits_home, winner_home) %>%
+              rename(hits = hits_home,
+                     win = winner_home)
+            ) %>%
+    group_by(hits) %>%
+    summarise(games = n(),
+              win_prob = mean(win)) %>%
+    ungroup() %>%
+    ggplot(data = .) +
+    geom_bar(mapping = aes(x = hits, y = games), stat = 'identity', fill = 'darkred') +
+    labs(x = 'Hits in Game',
+         y = 'Number of Game Occurrences')
+    ggsave(filename = './plots/hist_win_hits.png', width = 150, height = 100, units = 'mm', dpi = 500)
+
+
+
+# TODO: season winning record levels at team granularity, best to worst
+bind_rows(wl_logs %>%
+              select(team_visit, winner_visit) %>%
+              rename(team = team_visit,
+                     win = winner_visit),
+          wl_logs %>%
+              select(team_home, winner_home) %>%
+              rename(team = team_home,
+                     win = winner_home)
+          ) %>%
+    group_by(team) %>%
+    summarize(win_rate = mean(win)) %>%
+    ungroup() %>%
+    arrange(desc(win_rate)) %>%
+    mutate(order_index = as.numeric(rownames(.))) %>%
+    ggplot(data = .) +
+    geom_area(mapping = aes(x = order_index, y = win_rate), stat = 'identity', fill = 'darkblue') +
+    annotate(geom = 'segment', 
+             x = 1, y = 0.5, xend = 30, yend = 0.5, 
+             color = 'black', size = 1.5, linetype = 'longdash') +
+    scale_x_continuous(limits = c(1,30)) +
+    scale_y_continuous(limits = c(0,1)) +
+    theme_classic() +
+    theme(axis.text.x = element_blank(),
+          axis.title.x = element_blank(),
+          axis.line.x = element_blank(),
+          axis.ticks.x = element_blank()) +
+    labs(y = 'Win Record')
+    ggsave(filename = './plots/win_records.png', width = 150, height = 100, units = 'mm', dpi = 500)
+    
+
+
+
 
 
 
---
+++ scouting_report.R
@@ -0,0 +1,55 @@
+# TODO: create scouting report code
+
+
+
+# TODO: import all necessary data
+
+
+
+
+# TODO: map from player friendly names to ID (app_preprocess)
+
+
+# TODO: Pitching: classify pitchers by strengths in pitching and types of pitches capable
+
+
+
+# TODO: Batting: classify batters by weaknesses in Off-Base results, seek to reduce hitting capability
+# as much as possible. Quote outcomes in reduction of decimal anticipated hits.
+
+
+# TODO: Team: use batting function to expand to whole team of batters to quantify reduction in hits expected
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
